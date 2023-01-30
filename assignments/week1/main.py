04d5e4c888114bd131475bfcba1b0a4d3fe7893a
Zhao, Fenglong <fz13@illinois.edu>
2023-01-30 14:38:18 -0500

    restore main.py unchanged

--- assignments/week1/main.py
+++ assignments/week1/main.py
@@ -7,7 +7,6 @@ import numpy as np
 from typing import Tuple
 
 
-
 def get_housing_data() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Get the California housing data from sklearn.
@@ -60,7 +59,3 @@ def main():
 
     # Print the mean squared error:
     print("Mean squared error: {:.2f}".format(mse))
-
-
-if __name__ == "__main__":
-    main()
