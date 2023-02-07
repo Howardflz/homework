44e242c9a2965c10ab3ce48d7fafaee15f44e7a3
Zhao, Fenglong <fz13@illinois.edu>
2023-02-07 18:05:38 -0500

    first trial

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,16 +1,28 @@
 import torch
+import torch.nn as nn
 from typing import Callable
 
 
-class MLP:
+class MLP(nn.Module):
+    """
+        Structure of the network.
+
+        Arguments:
+            __init__: initialize the parameters.
+            forward: the forward process of NN
+
+        Returns:
+            Nothing.
+    """
+
     def __init__(
         self,
         input_size: int,
         hidden_size: int,
         num_classes: int,
         hidden_count: int = 1,
-        activation: Callable = torch.nn.ReLU,
-        initializer: Callable = torch.nn.init.ones_,
+        activation: Callable = nn.ReLU,
+        initializer: Callable = nn.init.ones_,
     ) -> None:
         """
         Initialize the MLP.
@@ -22,9 +34,28 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
 
-    def forward(self, x):
+        self.layers = nn.ModuleList()
+        self.hidden_size = hidden_size
+        #self.hidden_size = hidden_size
+
+        for i in range(hidden_count):
+          next_num_inputs = hidden_size 
+          self.layers += [nn.Linear(input_size, next_num_inputs)]
+          input_size = next_num_inputs
+
+        self.out = nn.Linear(input_size, num_classes)
+        self.batchNormal = nn.BatchNorm1d(self.hidden_size)
+        self.activation = activation()
+        self.dropout = nn.Dropout(0.5)
+        #self.initializer = initializer
+        for l in self.layers:
+            initializer(l.weight)
+
+        
+
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +65,12 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+
+        for layer in self.layers:
+            x = layer(x)
+            x = self.batchNormal(x)
+            x = self.activation(x)
+            x = self.dropout(x)
+
+        x = self.out(x) 
+        return x
