a6394e9a2cb5a55c083ddbcec80ce0f94c49803e
Zhao, Fenglong <fz13@illinois.edu>
2023-02-18 20:57:07 -0500

    method with stepLR

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,25 +6,24 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 10
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "gamma": 0.9,
+        "step_size": 100,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
-    transforms = Compose(
-        [
-            ToTensor(),
-        ]
-    )
+    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,7 +4,15 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Class for a new scheduler.
+
+    Have __init function with parameters and learning rate scheduler function.
+
+    """
+
+    # Non-default arguments should be before default arguments
+    def __init__(self, optimizer, step_size, gamma=0.9, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -13,6 +21,10 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+
+        self.gamma = gamma
+        self.step_size = step_size
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
@@ -21,4 +33,20 @@ class CustomLRScheduler(_LRScheduler):
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # return [i for i in self.base_lrs]
+        """
+        Revise based on LR scheduler.
+
+        After some step_size of epoch, we decay the learning rate by gamma and update the
+        new learning rate to the list.
+
+        """
+
+        l = []
+        cur = self.last_epoch
+        times = cur // self.step_size
+        for i in self.base_lrs:
+            lr = i * self.gamma**times
+            l.append(lr)
+
+        return l
